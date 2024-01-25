0dd3b1fb477c6fb22cda9d3928c2a88d5b6f0d58
Masud Rana <89310548+tuberboy@users.noreply.github.com>
2024-01-26 03:02:01 +0600

    Add files via upload

---
+++ Constants/Constants.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * mcHC - Constants
+ *
+ * @author Masud Rana
+ * @version 0.01
+ */
+
+namespace mcHC\Constants;
+
+class Constants {
+    const CAPTCHA_RELOAD_URL = 'https://www.google.com/recaptcha/api2/reload?k=';
+    const CAPTCHA_ENTERPRISE_RELOAD_URL = 'https://www.google.com/recaptcha/enterprise/reload?k=';
+}
